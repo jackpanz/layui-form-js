f55527df4a0a1e07d958b147b73afc31aa30dc41
jackpanz <panzijie50@gmail.com>
2021-11-29 17:29:58 +0800

    v 1.2 2021-11-29

---
+++ demo/js/init-data.js
@@ -0,0 +1,39 @@
+var sRows = [
+    {title: "title0", value: "value0"},
+    {title: "title1", value: "value1"},
+    {title: "title2", value: "value2"},
+];
+
+var rows = [
+    {title: 'radio0', value: '0',checked:true},
+    {title: 'radio1', value: '1'},
+    {title: 'radio2', value: '2'},
+    {title: 'radio3', value: '3'}
+];
+
+var cRows = [
+    {title: 'box0', name:'checkboxz.box0', value: '0'},
+    {title: 'box1', name:'checkboxz.box1', value: '1'},
+    {title: 'box2', name:'checkboxz.box2', value: '2'},
+    {title: 'box3', name:'checkboxz.box3', value: '3',checked:true}
+];
+
+var cRowsa = [
+    {title: 'box0', name:'checkboxa', value: 'box0'},
+    {title: 'box1', name:'checkboxa', value: 'box1'},
+    {title: 'box2', name:'checkboxa', value: 'box2'},
+    {title: 'box3', name:'checkboxa', value: 'box3',checked:true}
+];
+
+var sRows1 = [
+    {title: "", value: ""},
+    {title: "value1", value: "value1"},
+    {title: "value2", value: "value2"},
+];
+
+var vrows = [
+    {title: 'radio0', value: '0'},
+    {title: 'radio1', value: '1'},
+    {title: 'radio2', value: '2'},
+    {title: 'radio3', value: '3'}
+];
